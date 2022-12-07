dc7d85ab21859f3f175ca8638e8fc042619c1eba
Max Greive <maxgreive@googlemail.com>
2022-12-07 19:17:45 +0100

    update layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,9 @@
+import React from 'react';
+import Layout from './src/components/Layout';
+import './src/styles/global.css';
 import '@fontsource/roboto-condensed';
 import '@fontsource/roboto-condensed/latin-700.css';
+
+export function wrapPageElement({ element, props }) {
+  return <Layout {...props}>{element}</Layout>;
+}
